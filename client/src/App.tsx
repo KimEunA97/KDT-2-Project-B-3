c3078468ebd25367d3d7e97ce22e753743a7e6c2
jungheeeun <jungheeeun272@gmail.com>
2023-06-30 16:56:13 +0900

    /화장실 페이지 이동

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
-import { BtnContext, BtnContextData } from "./components/context/btnContext";
+import { BtnContext, BtnContextData } from './components/context/btnContext';
 import Header from './components/layout/header';
 import Custompage from './components/page/custompage';
 import EditButtonPage from './components/page/editButtonPage';
@@ -8,7 +8,7 @@ import LoginPage from './components/page/loginPage';
 import Mainpage from './components/page/mainPage';
 import ProtoTestPage from './components/page/testPageHer';
 const App = (): JSX.Element => {
-  console.log(BtnContextData)
+  console.log(BtnContextData);
   const location = useLocation();
   const [pageTitle, setPageTitle] = useState('');
   interface TitleMapping {
@@ -17,10 +17,10 @@ const App = (): JSX.Element => {
   const titleMapping: TitleMapping = {
     '/': '',
     '/main': '홈',
-    '/edit' : '단축 편집 버튼',
-    '/custom' : '카테고리 선택',
-    '/login' : '로그인',
-    '/signUp' : '회원가입'
+    '/edit': '단축 편집 버튼',
+    '/custom': '카테고리 선택',
+    '/login': '로그인',
+    '/signUp': '회원가입',
   };
   React.useEffect(() => {
     // switch (location.pathname) {
@@ -36,15 +36,15 @@ const App = (): JSX.Element => {
     <div>
       <Header title={pageTitle} />
       <BtnContext.Provider value={BtnContextData}>
-      <Routes>
-        <Route path="/" element={<ProtoTestPage />} />
-        <Route path="/main" element={<Mainpage />} />
-        <Route path="/custom" element={<Custompage />} />
-        <Route path="/login" element={<LoginPage />} />
-        {/* <Route path="/signUp" element={<SignUpPage />} /> */}
-        <Route path="/edit" element={<EditButtonPage />} />
-        <Route path="/edit/:buttonName" element={<EditButtonPage />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<ProtoTestPage />} />
+          <Route path="/main" element={<Mainpage />} />
+          <Route path="/custom" element={<Custompage />} />
+          <Route path="/login" element={<LoginPage />} />
+          {/* <Route path="/signUp" element={<SignUpPage />} /> */}
+          <Route path="/edit" element={<EditButtonPage />} />
+          <Route path="/edit/:buttonName" element={<EditButtonPage />} />
+        </Routes>
       </BtnContext.Provider>
     </div>
   );
--- client/src/components/services/Restroombutton.tsx
+++ client/src/components/services/Restroombutton.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { BtnContext } from '../context/btnContext';
+import { useNavigate } from 'react-router-dom';
 
 const Restroombutton = (): JSX.Element => {
-  function testEvent() {
+  const btnContextData = useContext(BtnContext);
+  const navigate = useNavigate();
+
+  function testEvent(buttonName: string) {
+    navigate(`/button/${buttonName}`);
     fetch('/test2').then((data) => {
-      console.log(data.json());
+      // console.log(data.json());
     });
   }
 
   return (
     <div>
       <button
-        onClick={testEvent}
+        onClick={() => testEvent('화장실')}
         style={{
           width: '30px',
           height: '30px',
