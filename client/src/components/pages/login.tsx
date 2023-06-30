ac71b09327dcea69ac11acd6194b2ee0460dde20
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-06-30 11:08:38 +0900

    새로 생성

---
+++ client/src/components/pages/login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LoginPage = (): JSX.Element => {
+  const style = {
+    width: '100%',
+    height: '300px',
+    backgroundColor: 'yellow',
+    border: '1px solid',
+  };
+
+  return <div style={style}></div>;
+};
+
+export default LoginPage;
