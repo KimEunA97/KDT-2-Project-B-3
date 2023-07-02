576cc6d95a0e991e96b3e2eebc2d2377e943489d
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-02 17:21:54 +0900

    선택한 요소의 색 변경

--- client/src/components/page/editButtonPage.tsx
+++ client/src/components/page/editButtonPage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 // import EditButton from '../common/editButtonSelect';
 import { DefaultBtnData } from "../context/btnContext";
@@ -14,6 +14,7 @@ interface ButtonParams {
 const EditButtonPage: React.FC = () => {
   const btnContextData = useContext(DefaultBtnData);
   const { value, image, name } = useParams<ButtonParams>();
+  const [selectedButton, setSelectedButton] = useState<number>(-1);
 
   if (!value || !image || !name) {
     // URL 파라미터가 없는 경우 처리
@@ -24,18 +25,51 @@ const EditButtonPage: React.FC = () => {
   console.log('Image:', image);
   console.log('Name:', name);
 
-  // 이하 컴포넌트 내용 작성
+  const handleButtonSelect = (buttonIndex: number) => {
+    setSelectedButton(buttonIndex);
+  };
+  
+  const handleConfirm  = ()=> {
+    if (selectedButton  !== -1) {
+      const updatedBtnContextData = btnContextData.map((button, index) => {
+        if (index === selectedButton ) {
+          // 선택한 버튼의 정보로 업데이트
+          return {
+            value: value,
+            image: image,
+            name: name,
+          };
+        }
+        return button;
+      });}
+
+  }
 
   return (
     <div>
-      <p><strong>어떤 버튼에 적용하겠습니까?</strong></p>
+    <p><strong>어떤 버튼에 적용하겠습니까?</strong></p>
 
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-
-      <button>확인</button>
+    <div
+      onClick={() => handleButtonSelect(0)}
+      style={{ backgroundColor: selectedButton === 0 ? 'navy' : '' }}
+    >
+      1
     </div>
+    <div
+      onClick={() => handleButtonSelect(1)}
+      style={{ backgroundColor: selectedButton === 1 ? 'navy' : '' }}
+    >
+      2
+    </div>
+    <div
+      onClick={() => handleButtonSelect(2)}
+      style={{ backgroundColor: selectedButton === 2 ? 'navy' : '' }}
+    >
+      3
+    </div>
+
+    <button onClick={handleConfirm}>확인</button>
+  </div>
   );
 };
 
