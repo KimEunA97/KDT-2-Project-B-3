5bd50b48c746f324707d1d960ef83e0de4b7b2dc
jungheeeun <jungheeeun272@gmail.com>
2023-07-03 21:48:06 +0900

    모달창 열릴때 뒷배경 투명도 설정

--- client/src/components/page/custompage.tsx
+++ client/src/components/page/custompage.tsx
@@ -1,18 +1,40 @@
 import React, { useState } from 'react';
 import Search from '../common/Search';
 import ButtonList from '../services/ButtonList';
+import YesNoModal from '../common/YesNoModal';
 
 const Custompage = (): JSX.Element => {
   const [searchTerm, setSearchTerm] = useState<string>('');
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false); // 모달 열림
 
   const handleSearch = (searchValue: string) => {
     setSearchTerm(searchValue);
+  };
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleConfirmModal = () => {
+    // 모달 확인 버튼 누를 때 실행할 동작
+    setIsModalOpen(false); // 모달 닫기
   };
 
   return (
     <div>
       <Search onSearch={handleSearch} />
       <ButtonList searchTerm={searchTerm} />
+      <YesNoModal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        onConfirm={handleConfirmModal}
+      />
+      {/* 모달이 열릴 때만 투명한 배경 표시 */}
+      {isModalOpen && <div className="modal" />}
     </div>
   );
 };
