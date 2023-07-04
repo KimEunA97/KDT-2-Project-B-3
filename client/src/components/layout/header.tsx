45777fa5e675b0760aa80ff6b27c18c7d3dc5d6a
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-04 12:47:36 +0900

    헤더에 버튼 등록 -> 버튼은 생겼으나 설치 이벤트 실행안됨

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-
+import PWABtn from "../services/PWA_InstallBtn";
 type HeaderProps = {
   title: string;
 };
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
+    <>
     <header>
       <h1>{title}</h1>
     </header>
+    <PWABtn />
+    </>
   );
 };
 
