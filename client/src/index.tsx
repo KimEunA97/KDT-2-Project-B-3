fa07a7a617ee61da427c78b4188e2809fc6371e3
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-05 18:26:05 +0900

    js와 다르게 ts는 export, import가 없으면 모듈로 인식 안한다는 충격적인 사실

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import reportWebVitals from '/reportWebVitals';
-import * as serviceWorkerRegistration from '/serviceWorkerRegistration';
-serviceWorkerRegistration.register();
+import * as serviceWorker from './service-worker';
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <BrowserRouter>
@@ -14,9 +12,8 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
