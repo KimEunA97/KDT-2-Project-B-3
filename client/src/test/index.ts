cd59000f99fa3cc14e2adf1eee682ab7e571476a
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-05 17:54:55 +0900

    manifest.json은 불러와지는 것 확인 -> 미들웨어 등록 및 요청에 응답 따로 해줌

--- client/src/test/index.ts
+++ client/src/test/index.ts
@@ -5,12 +5,17 @@ import path from 'path';
 const app = express();
 const root = path.join(__dirname, '..', '..');
 const rootPublic = path.join(root, '/public');
-
+app.use(express.static(root));
 app.use(express.static(rootPublic));
 
 // 서비스 워커 파일을 별도의 경로로 제공
 app.get('/service-worker.js', (req: Request, res: Response) => {
   res.sendFile(path.join(root,'src', 'service-worker.js'));
+});
+
+// manifest.json 요청에 대한 응답
+app.get('/manifest.json', (req: Request, res: Response) => {
+  res.sendFile(path.join(rootPublic, 'manifest.json'));
 });
 
 // 나머지 요청은 index.html 파일로 응답
