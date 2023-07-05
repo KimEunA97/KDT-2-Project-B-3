da4c6d4ddb3f46acbaab553d2ee7f829d2d38641
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-05 23:04:20 +0900

    pwa는 템플릿을 사용하는 걸로 하겠습니다.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,8 @@
-// 서비스 워커 등록
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/public/service-worker.js")
-      .then((registration) => {
-        console.log("Service Worker registered:", registration);
-      })
-      .catch((error) => {
-        console.log("Error registering Service Worker:", error);
-      });
-  });
-}
 import React from "react";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+serviceWorkerRegistration.register();
+
 
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
